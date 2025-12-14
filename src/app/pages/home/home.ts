38f40c248dab75d22a4f68550dd158dddc36c6ce
bhavana1312 <bhavana13sree@gmail.com>
2025-12-14 13:41:39 +0530

    add interceptor

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
+  standalone: true,
   imports: [],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
 export class Home {
+  constructor(private http: HttpClient) {}
 
+  testProtected() {
+    this.http
+      .get('http://localhost:9090/booking/history/bhavana13sree@gmail.com')
+      .subscribe((res) => console.log(res));
+  }
 }
