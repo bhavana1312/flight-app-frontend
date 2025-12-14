02785c706708db468e78ad74e58352c3d420793c
bhavana1312 <bhavana13sree@gmail.com>
2025-12-14 10:22:04 +0530

    add reactive login

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
+  loginForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      username: [''],
+      password: [''],
+    });
+  }
+
+  onSubmit() {
+    console.log(this.loginForm.value);
+  }
 }
