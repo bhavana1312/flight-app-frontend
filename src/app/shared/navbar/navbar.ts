37f8fe0e058effcdccab071db198ccf1c8aa3aa9
bhavana1312 <bhavana13sree@gmail.com>
2025-12-19 16:53:22 +0530

    modified navbar for logout

--- src/app/shared/navbar/navbar.ts
+++ src/app/shared/navbar/navbar.ts
@@ -2,19 +2,31 @@ import { Component } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { AuthService } from '../../services/auth';
 import { CommonModule } from '@angular/common';
+import { ConfirmModal } from '../confirm-modal/confirm-modal';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule, RouterModule], 
+  imports: [CommonModule, RouterModule, ConfirmModal],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
 export class Navbar {
+  showLogoutModal = false;
+
   constructor(public authService: AuthService, private router: Router) {}
 
-  logout() {
+  openLogout() {
+    this.showLogoutModal = true;
+  }
+
+  cancelLogout() {
+    this.showLogoutModal = false;
+  }
+
+  confirmLogout() {
     this.authService.logout();
+    this.showLogoutModal = false;
     this.router.navigate(['/login']);
   }
 }
