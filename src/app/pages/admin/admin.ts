38b737048aaf5e31cbdb5de129bdc931c93bfb43
bhavana1312 <bhavana13sree@gmail.com>
2025-12-21 09:13:33 +0530

    add inventory for admin

--- src/app/pages/admin/admin.ts
+++ src/app/pages/admin/admin.ts
@@ -1,69 +1,86 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-admin',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HttpClientModule, FormsModule],
   templateUrl: './admin.html',
   styleUrl: './admin.css',
 })
 export class Admin {
-  flights = [
-    {
-      flightNumber: '6E-131',
-      airline: 'Indigo',
-      from: 'Hyderabad',
-      to: 'Bengaluru',
-      date: '20-12-2025',
-      departure: '10:30 AM',
-      arrival: '12:00 PM',
-      seats: 60,
-      price: 3500,
-    },
-    {
-      flightNumber: 'AI-258',
-      airline: 'Air India',
-      from: 'Hyderabad',
-      to: 'Bengaluru',
-      date: '20-12-2025',
-      departure: '13:15 AM',
-      arrival: '14:45 PM',
-      seats: 90,
-      price: 4500,
-    },
-    {
-      flightNumber: 'AI-203',
-      airline: 'Air India',
-      from: 'Bengaluru',
-      to: 'Hyderabad',
-      date: '20-12-2025',
-      departure: '10:30 AM',
-      arrival: '11:45 AM',
-      seats: 120,
-      price: 4599,
-    },
-    {
-      flightNumber: '6E-512',
-      airline: 'IndiGo',
-      from: 'Chennai',
-      to: 'Delhi',
-      date: '20-12-2025',
-      departure: '02:15 PM',
-      arrival: '05:10 PM',
-      seats: 90,
-      price: 5299,
-    },
-    {
-      flightNumber: 'SG-889',
-      airline: 'SpiceJet',
-      from: 'Mumbai',
-      to: 'Goa',
-      date: '20-12-2025',
-      departure: '08:00 AM',
-      arrival: '09:10 AM',
-      seats: 60,
-      price: 3499,
-    },
-  ];
+  successMsg = '';
+  errorMsg = '';
+
+  inventory = {
+    airlineName: '',
+    airlineLogoUrl: 'https://example.com/logo.png',
+    fromPlace: '',
+    toPlace: '',
+    departureDateTime: '',
+    arrivalDateTime: '',
+    aircraftModel: 'A320',
+    businessSeats: 0,
+    businessSeatPrice: 0,
+    economySeats: 0,
+    economySeatPrice: 0,
+  };
+
+  constructor(private http: HttpClient) {}
+
+  addInventory() {
+    this.successMsg = '';
+    this.errorMsg = '';
+
+    this.http.post('http://localhost:9090/flight/airline/inventory/add', this.inventory).subscribe({
+      next: () => {
+        this.successMsg = 'Flight inventory added successfully';
+        this.resetForm();
+      },
+      error: (err) => {
+        if (err.error) {
+          if (typeof err.error === 'string') {
+            this.errorMsg = err.error;
+          } else if (err.error.message) {
+            this.errorMsg = err.error.message;
+          } else {
+            this.errorMsg = 'Invalid request. Please check inputs.';
+          }
+        } else {
+          this.errorMsg = 'Server error. Please try again later.';
+        }
+      },
+    });
+  }
+
+  resetForm() {
+    this.inventory = {
+      airlineName: '',
+      airlineLogoUrl: 'https://example.com/logo.png',
+      fromPlace: '',
+      toPlace: '',
+      departureDateTime: '',
+      arrivalDateTime: '',
+      aircraftModel: 'A320',
+      businessSeats: 0,
+      businessSeatPrice: 0,
+      economySeats: 0,
+      economySeatPrice: 0,
+    };
+  }
+  isFormValid() {
+    return (
+      this.inventory.airlineName &&
+      this.inventory.fromPlace &&
+      this.inventory.toPlace &&
+      this.inventory.departureDateTime &&
+      this.inventory.arrivalDateTime &&
+      this.inventory.businessSeats > 0 &&
+      this.inventory.businessSeatPrice > 0 &&
+      this.inventory.economySeats > 0 &&
+      this.inventory.economySeatPrice > 0
+    );
+  }
 }
